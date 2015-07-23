0e650a52c51cbc1ef35e8a75d0bac3f7efcf1a2c
silves05 <steven.silvestri@mountsinai.org>
2015-07-23 13:02:06 -0400

    remvoed newrelic

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 /**
  * Created by steve on 7/22/15.
  */
-require('./newrelic');
 var gzippo = require('gzippo');
 var express = require('express');
 //var path = require('path');
@@ -10,7 +9,6 @@ var logger = require('morgan');
 
 app.use(logger('dev'));
 app.use(gzippo.staticGzip(__dirname + '/dist'));
-//app.use(express.static(path.join(__dirname + 'views')));
 app.listen(process.env.PORT || 8080);
 
 
